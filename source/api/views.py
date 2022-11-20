4365c266fa855d568ea39c636470ba66c66998fe
Адильбек Джарасов <dzharasov97@gmail.com>
2022-11-20 16:45:11 +0600

    Реализован UpdateView API для задач методом PUT

--- source/api/views.py
+++ source/api/views.py
@@ -1,10 +1,9 @@
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from api.serializers import TaskDetailSerializer
 from rest_framework.response import Response
 from webapp.models import Task
 from rest_framework import status
-from api.serializers import ProjectDetailSerializer
-from webapp.models import Project
 
 
 class TaskDetailView(APIView):
@@ -18,15 +17,10 @@ class TaskDetailView(APIView):
                 "message": "Задача не найдена."
             }, status=404)
 
-
-class ProjectDetailView(APIView):
-    def get(self, request, pk):
-        try:
-            project = Project.objects.get(pk=pk)
-            serializer = ProjectDetailSerializer(project)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except:
-            return Response({
-                "message": "Проект не найден."
-            }, status=404)
+    def put(self, request, *args, pk, **kwargs):
+        task = get_object_or_404(Task, pk=pk)
+        serializer = TaskDetailSerializer(data=request.data, instance=task)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
