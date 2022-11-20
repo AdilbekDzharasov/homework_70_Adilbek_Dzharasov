a8bfbc2231964c12b0f00e39b6449b605b2e11d3
Адильбек Джарасов <dzharasov97@gmail.com>
2022-11-20 16:47:53 +0600

    Реализованы DeleteView API для проектов методом DELETE, UpdateView API для проектов методом PUT

--- source/api/views.py
+++ source/api/views.py
@@ -4,6 +4,8 @@ from api.serializers import TaskDetailSerializer
 from rest_framework.response import Response
 from webapp.models import Task
 from rest_framework import status
+from api.serializers import ProjectDetailSerializer
+from webapp.models import Project
 
 
 class TaskDetailView(APIView):
@@ -29,5 +31,31 @@ class TaskDetailView(APIView):
         task.delete()
         return Response({
             "message": f"Задача {pk} удалена."
+        }, status=204)
+
+
+class ProjectDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            project = Project.objects.get(pk=pk)
+            serializer = ProjectDetailSerializer(project)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            return Response({
+                "message": "Проект не найден."
+            }, status=404)
+
+    def put(self, request, *args, pk, **kwargs):
+        project = get_object_or_404(Project, pk=pk)
+        serializer = ProjectDetailSerializer(data=request.data, instance=project)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, *args, pk, **kwargs):
+        project = get_object_or_404(Project, pk=pk)
+        project.delete()
+        return Response({
+            "message": f"Проект {pk} удален."
         }, status=204)
 
